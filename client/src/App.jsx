2fcef0d1ab024092b7f65cbda46d2e86f39de624
Ayisha Ilyas <ayishailyas21@gmail.com>
2024-06-03 02:05:08 +0530

    created footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './pages/Projects';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 
@@ -23,6 +24,7 @@ export default function App() {
       <Route path="/projects" element={<Projects />}></Route>
 
      </Routes>
+     <Footer />
     </BrowserRouter>
   )
 }
