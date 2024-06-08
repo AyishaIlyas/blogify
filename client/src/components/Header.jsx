4d53284710a47fa895d5b53883dc2bf104a030bb
Ayisha Ilyas <ayishailyas21@gmail.com>
2024-06-08 16:31:19 +0530

    added dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import {useSelector} from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
+
 
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const {currentUser} = useSelector(state => state.user);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [activeSection, setActiveSection] = useState(path);
+  const { theme } = useSelector((state) => state.theme);
 
   const handleDropdownToggle = () => {
     setDropdownOpen(!dropdownOpen);
@@ -50,8 +54,8 @@ export default function Header() {
       {/* Items Aligned Between Search and Sign In */}
       <div className="flex gap-2 md:order-2 lg:flex lg:items-center">
         {/* Moon Icon */}
-        <Button className="w-12 h-10" color="gray" pill>
-          <FaMoon />
+        <Button className="w-12 h-10" color="gray" pill onClick={() => dispatch(toggleTheme())}>
+        {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
 
         {/* Navbar Links (Visible on Larger Screens) */}
